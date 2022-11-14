3712b56483500f72a5f5ac2b1f3fe109cfd267d6
william kreuch <williamkreuch@gmail.com>
2022-11-14 18:30:14 -0300

    Swagger

--- parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -2,11 +2,8 @@ package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
